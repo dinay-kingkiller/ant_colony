82b851e96aacb942b99376afcd2b121331408c87
dinay-kingkiller <dinay.kingkiller@gmail.com>
2023-02-23 23:05:07 -0800

    Finished most of the exploring function of the ant node.

--- src/ants/src/ant.cpp
+++ src/ants/src/ant.cpp
@@ -1,20 +1,54 @@
-// Generate a new ant publisher.
+// TODO MOVE TO CONFIG
+#define BUFFER_SIZE 100000
+#define VERTEX_COUNT 256
+
 #include <string>
+#include <vector>
 
 #include "ros/ros.h"
-#include "ants/pheromone.h"
-#include "ants/explore.h"
+#include "ant_colony/pheromone.h"
+#include "ant_colony/where_next.h"
 
 
 int main(int argc, char **argv) {
+  // Setup this ant
   std::string name = argv[1];
   ros::init(argc, argv, name);
-  ros::NodeHandle ant;
-  ros::Publisher report = ant.advertise<ants::pheromone>("checked", BUFFER_SIZE);
-  ros::Rate travel_rate(1); // Ants travel one unit per second.
+
+  // Setup communication with ROS
+  ros::NodeHandle nh;
+  ros::Publisher reporter = nh.advertise<ant_colony::pheromone>("paths_checked", BUFFER_SIZE);
+  ros::ServiceClient lost_ant = nh.serviceClient<ant_colony::where_next>("where_next");
+  ant_colony::where_next where_next_srv;
+  
+  bool exploring = true;
+  int start_vertex = 0;
+  int current_vertex = start_vertex;
+  int goal_vertex = VERTEX_COUNT;
+  std::vector<int> tour;
+  std::vector<int> path_length;
+  
   while (ros::ok()) {
     if (exploring) {
-      if (next_node == goal_node) {
-	}
+      // The ant wants to know where to go next.
+      where_next_srv.start_vertex = current_vertex;
+      if (not lost_ant.call(where_next_srv)) {
+	ROS_ERROR(name+" could not talk to where_next service.");
+	return 1;
+      }
+      ros::Duration(where_next_srv.response.travel_time).sleep();
+      current_vertex = where_next_srv.response.next_vertex;
+      if (current_vertex == goal_vertex) {
+	exploring = false;
+      }
     }
+    else {
+      // The ant is returning, spreading the good news!
+      if (current_vertex == start_vertex) {
+	exploring = true;
+      }
+    }
+    
+  }
+  return 0;
 }
