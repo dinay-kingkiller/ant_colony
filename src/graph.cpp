d959c044f084d8f6046f28e4abb409aebe4590dd
dinay-kingkiller <dinay-kingkiller@gmail.com>
2023-02-25 19:40:22 -0800

    Added flat graph generator

--- src/graph.cpp
+++ src/graph.cpp
@@ -28,8 +28,9 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <cmath>
+#include <vector>
 
-void GenerateRandomGraph(std::vector<std::vector<float>>& graph, int vertex_c, float max_weight) {
+void GenerateRandomGraph(std::vector<std::vector<int>>& graph, int vertex_c, int max_weight) {
   // Generates a connected, undirected, simple, weighted graph.
   //   For any two vertices in a connected graph, there is a path to connect them.
   //   An undirected graph is one where the weight of an edge does not depend on the direction
@@ -37,15 +38,15 @@ void GenerateRandomGraph(std::vector<std::vector<float>>& graph, int vertex_c, f
   //   A graph is simple if two edges cannot share the same vertices AND
   //   the graph does not contain loops: graph[i][i] == 0
   //   The weight of the edge i->j is stored in graph[i][j]
-  // std::vector<std::vector<float>>& graph:
-  //   The adjacency matrix of the graph. For missing edges: graph[i][j] == 0
+  // std::vector<std::vector<int>>& graph:
+  //   The adjacency matrix of the graph. For missing edges: graph[i][j]
   // int vertex_c:
   //   The size of the graph: how many vertices does it contain?
   //   edge_c (the number of edges) is randomized based on vertex_c
-  // float max_weight:
+  // int max_weight:
   //   The max edge weight.
 
-  graph.resize(size, std::vector<float>(vertex_c, 0.0));
+  graph.resize(vertex_c, std::vector<int>(vertex_c, 0));
 
   // Generate a random edge count.
   int max_edges = vertex_c * (vertex_c-1) / 2;
@@ -56,7 +57,7 @@ void GenerateRandomGraph(std::vector<std::vector<float>>& graph, int vertex_c, f
   int vertex;
   for (int i = 1; i < vertex_c; ++i) {
     vertex = rand() % i;
-    graph[vertex][i] = rand() % max_weight;
+    graph[vertex][i] = rand() % max_weight + 1;
     graph[i][vertex] = graph[vertex][i];
   }
 
@@ -74,13 +75,13 @@ void GenerateRandomGraph(std::vector<std::vector<float>>& graph, int vertex_c, f
     }
     from = location/vertex_c;
     to = location/vertex_c;
-    graph[from][to] = rand() % max_weight;
+    graph[from][to] = rand() % max_weight + 1;
     graph[to][from] = graph[from][to];
   }
 }
 
-void GenerateCompleteGraph(std::vector<std::vector<float>>& graph, int vertex_c, float max_weight) {  // Generates a connected, undirected, simple, weighted, complte graph.
-  //   For any two vertices in a connected graph, there is a path to connect them.
+void GenerateCompleteGraph(std::vector<std::vector<int>>& graph, int vertex_c, int max_weight) {
+  // Generates an undirected, simple, weighted, complete graph.
   //   An undirected graph is one where the weight of an edge does not depend on the direction
   //     graph[i][j] == graph[j][i]
   //   A graph is simple if two edges cannot share the same vertices AND
@@ -88,45 +89,76 @@ void GenerateCompleteGraph(std::vector<std::vector<float>>& graph, int vertex_c,
   //   The weight of the edge i->j is stored in graph[i][j]
   //   A graph is complete if every vertex has an edge to every other vertex
   //     graph[i][j] != 0 for all i, j except if i==j.
-  // std::vector<std::vector<float>>& graph:
-  //   The adjacency matrix of the graph. For missing edges: graph[i][j] == 0
+  // std::vector<std::vector<int>>& graph:
+  //   The adjacency matrix of the graph.
   // int vertex_c:
   //   The size of the graph: how many vertices does it contain?
   //   edge_c (the number of edges) is randomized based on vertex_c
-  // float max_weight:
+  // int max_weight:
   //   The max edge weight.
 
-  graph.resize(size, std::vector<float>(size, 0.0));
+  graph.resize(vertex_c, std::vector<int>(vertex_c, 0));
   for (int i = 0; i < vertex_c; ++i) {
     for (int j = i+1; j < vertex_c; ++j) {
-      graph[i][j] = rand() % max_weight;
+      graph[i][j] = rand() % max_weight + 1;
       graph[j][i] = graph[i][j];
     }
   }
 }
 
-void GenerateCompleteDigraph(std::vector<std::vector<float>>& graph, int vertex_c, float max_weight) {
-  // Generates a connected, undirected, simple, weighted, complte graph.
-  //   For any two vertices in a connected graph, there is a path to connect them.
-  //   An undirected graph is one where the weight of an edge does not depend on the direction
-  //     graph[i][j] == graph[j][i]
+void GenerateCompleteDigraph(std::vector<std::vector<int>>& graph, int vertex_c, int max_weight) {
+  // Generates a directed, simple, weighted, complete graph.
+  //   A directed graph is one where the weight of an edge might depend on direction.
   //   A graph is simple if two edges cannot share the same vertices AND
   //   the graph does not contain loops: graph[i][i] == 0
   //   The weight of the edge i->j is stored in graph[i][j]
   //   A graph is complete if every vertex has an edge to every other vertex
   //     graph[i][j] != 0 for all i, j except if i==j.
-  // std::vector<std::vector<float>>& graph:
-  //   The adjacency matrix of the graph. For missing edges: graph[i][j] == 0
+  // std::vector<std::vector<int>>& graph:
+  //   The adjacency matrix of the graph.
   // int vertex_c:
   //   The size of the graph: how many vertices does it contain?
   //   edge_c (the number of edges) is randomized based on vertex_c
-  // float max_weight:
-  //   The max edge weight.  graph.resize(size, std::vector<float>(size, 0.0));
+  // int max_weight:
+  //   The max edge weight.
+
+  graph.resize(vertex_c, std::vector<int>(vertex_c, 0));
   for (int i = 0; i < vertex_c; ++i) {
     for (int j = 0; j < vertex_c; ++j) {
       if (i!=j) {
-	graph[i][j] = rand() % max_weight;
+	graph[i][j] = rand() % max_weight + 1;
       }
+    }
+  }
+}
+
+void GenerateFlatGraph(std::vector<int>& coord_x, std::vector<int>& coord_y, std::vector<std::vector<int>>& graph, int vertex_c, int size_x, int size_y) {
+  // Generates an easily plotted complete graph.
+  // std::vector<int>& coord_x / std::vector<int>& coord_y
+  //   randomized coordinates
+  // std::vector<std::vector<int>>& graph:
+  //   The adjacency matrix of the graph.
+  //   The weight of each edge is the quadrature between two points.
+  //   (x_1-x_2)^2 + (y_1-y_2)^2
+  // int vertex_c:
+  //   The size of the graph: how many vertices does it contain?
+  //   edge_c (the number of edges) is randomized based on vertex_c
+  // int size_x / int size_y:
+  //   The dimensions of the map.
+
+  graph.resize(vertex_c, std::vector<int>(vertex_c, 0));
+  coord_x.resize(vertex_c, 0);
+  coord_y.resize(vertex_c, 0);
+  int diff_x;
+  int diff_y;
+  for (int i = 0; i < vertex_c; ++i) {
+    coord_x[i] = rand() % size_x;
+    coord_y[i] = rand() % size_y;
+    for (int j = 0; j < i; ++j) {
+      diff_x = coord_x[i] - coord_x[j];
+      diff_y = coord_y[i] - coord_y[j];
+      graph[i][j] = diff_x*diff_x + diff_y*diff_y;
+      graph[j][i] = graph[i][j];
     }
   }
 }
