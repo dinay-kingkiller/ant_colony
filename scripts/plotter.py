140044b53c7d62bc2fa05dd6791a2d14dd7accec
dinay-kingkiller <dinay.kingkiller@gmail.com>
2023-03-03 23:35:42 -0800

    added plot_data.msg

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -5,15 +5,16 @@ import rospy
 from matplotlib.pyplot import plot
 from numpy import reshape
 
+from ant_colony.msg import SOME_MSG
+
 def update(data):
-    pheromones = reshape(data.pheromones, (VertexCount, VertexCount))
-    for blah in blahblah:
+    for p1, p2, val in zip(zip(data.x1, data.y1), zip(data.x2, data.y2), data.value):
         plot(x, y)
 
     
 def plotter():
     rospy.init_node("plotter")
-    rospy.Subscriber(SOMETOPIC, SOME_MSG, update)
+    rospy.Subscriber("plot_data", PlotPheromones, update)
     rospy.spin()
 
 if __name__ == "__main__":
