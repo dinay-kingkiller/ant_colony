3e7f8c2919febc11cc6982e3b3e24ee6dc841489
dinay-kingkiller <dinay.kingkiller@gmail.com>
2023-03-03 23:54:52 -0800

    Updated plotter

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -1,21 +1,48 @@
 #!/usr/bin/env python
 
+# BSD 3-Clause License
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 import rospy
 
 from matplotlib.pyplot import plot
-from numpy import reshape
 
-from ant_colony.msg import SOME_MSG
+from ant_colony.msg import PlotData
 
 def update(data):
-    for p1, p2, val in zip(zip(data.x1, data.y1), zip(data.x2, data.y2), data.value):
-        plot(x, y)
+    for from_p, to_p, value in zip(zip(data.from_x, data.from_y),
+                                   zip(data.to_x, data.to_y),
+                                   data.values):
+        plot(from_p, to_p, hsv(value))
 
-    
-def plotter():
+
+if __name__ == "__main__":
     rospy.init_node("plotter")
     rospy.Subscriber("plot_data", PlotPheromones, update)
     rospy.spin()
-
-if __name__ == "__main__":
     plotter()
