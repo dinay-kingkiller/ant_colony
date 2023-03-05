1b6e5e50ead41881f9c723f35bc0ad86389e492e
dinay-kingkiller <dinay.kingkiller@gmail.com>
2023-03-04 19:19:43 -0800

    Added Incomplete Graph generation.

--- src/graph.cpp
+++ src/graph.cpp
@@ -115,4 +115,57 @@ void GenCompleteGraph(std::vector<float>& coord_x, std::vector<float>& coord_y, 
 }
 
 void GenIncompleteGraph(std::vector<float>& coord_x, std::vector<float>& coord_y, std::vector<std::vector<float>>& graph, int vertex_c, float size_x, float size_y) {
+  srand(time(NULL));
+  graph.resize(vertex_c, std::vector<float>(vertex_c, 0.0));
+  coord_x.resize(vertex_c, 0.0);
+  coord_y.resize(vertex_c, 0.0);
+
+  int diff_x;
+  int diff_y;
+  
+  // Generate a random edge count.
+  int max_edges = vertex_c * (vertex_c-1) / 2;
+  int min_edges = vertex_c - 1;
+  int edge_c = rand() % (max_edges-min_edges) + min_edges;
+
+  // Generate vertices.
+  for (int i = 0; i < vertex_c; ++i) {
+    coord_x[i] = rand() * size_x / RAND_MAX;
+    coord_y[i] = rand() * size_y / RAND_MAX;
+  }
+  
+  // Generate a connected graph.
+  for (int i = 1; i < vertex_c; ++i) {
+    int j = rand() % i;
+    diff_x = coord_x[i] - coord_x[j];
+    diff_y = coord_y[i] - coord_y[j];
+    graph[i][j] = sqrt(diff_x*diff_x + diff_y*diff_y);
+    graph[j][i] = graph[j][i];
+  }
+
+  // Generate the remaining edges.
+  int location;
+  int from;
+  int to;
+  int remaining; // remaining open locations in adjacency matrix.
+  for (int i = 0; i < edge_c-vertex_c; ++i) {
+    remaining = vertex_c*vertex_c - 3*vertex_c + 2 - 2*i;
+    for (location = rand() % remaining; location < vertex_c*vertex_c; ++location) {
+      if (location/vertex_c == location%vertex_c) {
+	continue;
+      }
+      else if (graph[location/vertex_c][location%vertex_c]!=0) {
+	continue;
+      }
+      else {
+	from = location/vertex_c;
+	to = location%vertex_c;
+	diff_x = coord_x[from] - coord_x[to];
+	diff_y = coord_y[from] - coord_y[to];
+	graph[from][to] = sqrt(diff_x*diff_x + diff_y*diff_y);
+	graph[to][from] = graph[from][to];
+	break;
+      }
+    }
+  }
 }
